bbfc956991653d26a57384e16e2cd97f0fa563f2
victor <dot@mymngmt.com>
2017-01-23 13:47:26 -0500

    Module enabling file

--- install-dev-victor.sh
+++ install-dev-victor.sh
@@ -4,16 +4,12 @@
 
 magentoRootPath="/Users/$USER/Code/magento19/root"
 
-rm $magentoRootPath/app/etc/modules/Wfn_Tagger.xml
-#rm -R $magentoRootPath/app/code/local/Wfn/Tagger/
-#rm -R $magentoRootPath/app/design/adminhtml/default/default/template/wfn_tagger
-rm $magentoRootPath/app/design/adminhtml/default/default/layout/wfn_tagger.xml
-rm -R $magentoRootPath/js/wfn_tagger
-rm -R $magentoRootPath/skin/adminhtml/default/default/wfn_tagger
+rm $magentoRootPath/app/etc/modules/Wfn_ConcardisPay.xml
+#rm -R $magentoRootPath/app/code/local/Wfn/ConcardisPay/
+#rm -R $magentoRootPath/app/design/frontend/base/default/template/wfn_concardispay
+#rm -R $magentoRootPath/app/design/adminhtml/default/default/template/wfn_concardispay
 
-cp -R ./src/app/etc/modules/Wfn_Tagger.xml $magentoRootPath/app/etc/modules/Wfn_Tagger.xml
-cp -R ./src/app/code/local/Wfn/Tagger/ $magentoRootPath/app/code/local/Wfn/Tagger/
-cp -R ./src/app/design/adminhtml/default/default/template/wfn_tagger/ $magentoRootPath/app/design/adminhtml/default/default/template/wfn_tagger
-cp -R ./src/app/design/adminhtml/default/default/layout/wfn_tagger.xml $magentoRootPath/app/design/adminhtml/default/default/layout/wfn_tagger.xml
-cp -R ./src/js/wfn_tagger/ $magentoRootPath/js/wfn_tagger
-cp -R ./src/skin/adminhtml/default/default/wfn_tagger/ $magentoRootPath/skin/adminhtml/default/default/wfn_tagger
+cp -R ./src/app/etc/modules/Wfn_ConcardisPay.xml $magentoRootPath/app/etc/modules/Wfn_ConcardisPay.xml
+#cp -R ./src/app/code/local/Wfn/ConcardisPay/ $magentoRootPath/app/code/local/Wfn/ConcardisPay/
+#cp -R ./src/app/design/frontend/base/default/template/wfn_concardispay/ $magentoRootPath/app/design/frontend/base/default/template/wfn_concardispay/
+#cp -R ./src/app/design/adminhtml/default/default/template/wfn_concardispay/ $magentoRootPath/app/design/adminhtml/default/default/template/wfn_concardispay/
