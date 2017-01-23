8167a3ee56993aa9c35b17745e84cfdbeb5eee63
victor <dot@mymngmt.com>
2017-01-23 15:30:10 -0500

    Frontend payment form

--- src/app/code/local/Wfn/ConcardisPay/Model/Method.php
+++ src/app/code/local/Wfn/ConcardisPay/Model/Method.php
@@ -7,5 +7,15 @@ class Wfn_ConcardisPay_Model_Method extends Mage_Payment_Model_Method_Abstract
     /**
      * {@inheritdoc}
      */
-    protected $_code = 'wfn_concardis';
+    protected $_code = 'wfn_concardispay';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $_formBlockType = 'wfnconcardispay/form';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $_infoBlockType = 'wfnconcardispay/info';
 }
