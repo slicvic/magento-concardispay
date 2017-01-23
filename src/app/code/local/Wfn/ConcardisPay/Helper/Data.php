6032a94da9af7e9c7fb98e63defda124be9c8b72
victor <dot@mymngmt.com>
2017-01-23 14:42:18 -0500

    Add system configuration settings

---
+++ src/app/code/local/Wfn/ConcardisPay/Helper/Data.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Data helper.
+ */
+class Wfn_ConcardisPay_Helper_Data extends Mage_Core_Helper_Abstract
+{
+    //
+}
---
+++ src/app/code/local/Wfn/ConcardisPay/Model/Method.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Payment method model.
+ */
+class Wfn_ConcardisPay_Model_Method extends Mage_Payment_Model_Method_Abstract
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected $_code = 'wfn_concardis';
+}
