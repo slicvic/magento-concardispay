949eb44436db93b2c0e824b85497427f7cd2f1ca
victor <dot@mymngmt.com>
2017-01-23 15:42:32 -0500

    Extending CC method model

--- install-dev-victor.sh
+++ install-dev-victor.sh
@@ -6,10 +6,6 @@ magentoRootPath="/Users/$USER/Code/magento19/root"
 
 rm $magentoRootPath/app/etc/modules/Wfn_ConcardisPay.xml
 rm -R $magentoRootPath/app/code/local/Wfn/ConcardisPay/
-rm -R $magentoRootPath/app/design/frontend/base/default/template/wfn_concardispay
-rm -R $magentoRootPath/app/design/adminhtml/default/default/template/wfn_concardispay
 
 cp -R ./src/app/etc/modules/Wfn_ConcardisPay.xml $magentoRootPath/app/etc/modules/Wfn_ConcardisPay.xml
 cp -R ./src/app/code/local/Wfn/ConcardisPay/ $magentoRootPath/app/code/local/Wfn/ConcardisPay/
-cp -R ./src/app/design/frontend/base/default/template/wfn_concardispay/ $magentoRootPath/app/design/frontend/base/default/template/wfn_concardispay/
-cp -R ./src/app/design/adminhtml/default/default/template/wfn_concardispay/ $magentoRootPath/app/design/adminhtml/default/default/template/wfn_concardispay/
--- src/app/code/local/Wfn/ConcardisPay/Model/Method.php
+++ src/app/code/local/Wfn/ConcardisPay/Model/Method.php
@@ -2,20 +2,10 @@
 /**
  * Payment method model.
  */
-class Wfn_ConcardisPay_Model_Method extends Mage_Payment_Model_Method_Abstract
+class Wfn_ConcardisPay_Model_Method extends Mage_Payment_Model_Method_Cc
 {
     /**
      * {@inheritdoc}
      */
     protected $_code = 'wfn_concardispay';
-
-    /**
-     * {@inheritdoc}
-     */
-    protected $_formBlockType = 'wfnconcardispay/form';
-
-    /**
-     * {@inheritdoc}
-     */
-    protected $_infoBlockType = 'wfnconcardispay/info';
 }
