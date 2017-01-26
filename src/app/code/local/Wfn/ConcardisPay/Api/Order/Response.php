a0071e1e0c07a7f587a49f737d0507025f3bfd23
victor <dot@mymngmt.com>
2017-01-25 23:32:00 -0500

    Rename constant

--- src/app/code/local/Wfn/ConcardisPay/Api/Order/Response.php
+++ src/app/code/local/Wfn/ConcardisPay/Api/Order/Response.php
@@ -4,16 +4,16 @@
  */
 class Wfn_ConcardisPay_Api_Order_Response extends Wfn_ConcardisPay_Api_Abstract_Response
 {
-    const STATUS_AUTHORIZED       = 5;
-    const STATUS_PAYMENT_ACCEPTED = 9;
+    const STATUS_AUTHORIZED        = 5;
+    const STATUS_PAYMENT_REQUESTED = 9;
 
     /**
-     * Check if transaction was approved.
+     * Check if transaction is approved.
      *
      * @return bool
      */
     public function isApproved()
     {
-        return ($this->status == self::STATUS_AUTHORIZED || $this->status == self::STATUS_PAYMENT_ACCEPTED);
+        return ($this->status == self::STATUS_AUTHORIZED || $this->status == self::STATUS_PAYMENT_REQUESTED);
     }
 }
